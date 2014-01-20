b038310f58dce82483150bde200d12a04d7d57fa
Dietrich Featherston <d@github.com>
2014-01-20 00:02:35 -0800

    simple test sketch

---
+++ src/main/java/com/d2fn/passage/sketches/TestSketch.java
@@ -0,0 +1,27 @@
+package com.d2fn.passage.sketches;
+
+import org.measures.passage.Sketch;
+
+/**
+ * TestSketch
+ * @author Dietrich Featherston
+ */
+public class TestSketch extends Sketch {
+
+    @Override
+    public void setup() {
+        initSize(P2D);
+        debug();
+    }
+
+    @Override
+    public void renderFrame() {
+        background(0);
+        colorMode(RGB, 255);
+        stroke(100, 100, 100);
+        line(0, 0, width, height);
+        colorMode(HSB, 360);
+        fill(frameCount%360, 360, 360);
+        ellipse(mouseX, mouseY, 10, 20);
+    }
+}
