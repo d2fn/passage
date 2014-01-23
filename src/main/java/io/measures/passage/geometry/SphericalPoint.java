f7954abf202c99c2d0365ccbd1e1858d67da1e43
Dietrich Featherston <d@github.com>
2014-01-22 18:38:04 -0800

    build SphericalPoint from xyz

--- src/main/java/io/measures/passage/geometry/SphericalPoint.java
+++ src/main/java/io/measures/passage/geometry/SphericalPoint.java
@@ -39,5 +39,18 @@ public class SphericalPoint implements Projectable3D {
     public float x() { return r * sin(theta) * cos(phi); }
     public float y() { return r * sin(theta) * sin(phi); }
     public float z() { return r * cos(theta); }
+
+    public static SphericalPoint fromXYZ(float x, float y, float z) {
+        float r = dist(0, 0, 0, x, y, z);
+        float phi = atan(y/x);
+        if(phi > TWO_PI) {
+            phi = phi % TWO_PI;
+        }
+        while(phi < 0) {
+            phi = TWO_PI + phi;
+        }
+        float theta = acos(z/r);
+        return new SphericalPoint(r, phi, theta);
+    }
 }
 
