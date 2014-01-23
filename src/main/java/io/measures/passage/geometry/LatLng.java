0fa86fc2ca97d02d701ab60689d476358bab1ffe
Dietrich Featherston <d@github.com>
2014-01-23 02:16:03 -0800

    latitude/longitude bean class

--- src/main/java/io/measures/passage/geometry/LatLng.java
+++ src/main/java/io/measures/passage/geometry/LatLng.java
@@ -6,26 +6,14 @@ import static io.measures.passage.Sketch.*;
  * LatLng
  * @author Dietrich Featherston
  */
-public class LatLng implements Projectable3D {
+public class LatLng {
 
-    private final float r;
     private final float lat;
     private final float lng;
 
-    private final SphericalPoint p;
-
-    public LatLng(float r, float lat, float lng) {
-        this.r = r;
+    public LatLng(float lat, float lng) {
         this.lat = lat;
         this.lng = lng;
-
-        float phi = radians(lng);
-        float theta = map(radians(lat), -PI/2, PI/2, 0, PI);
-        this.p = new SphericalPoint(r, phi, theta);
-    }
-
-    public float r() {
-        return r;
     }
 
     public float lat() {
@@ -36,18 +24,9 @@ public class LatLng implements Projectable3D {
         return lng;
     }
 
-    @Override
-    public float x() {
-        return p.x();
-    }
-
-    @Override
-    public float y() {
-        return p.y();
-    }
-
-    @Override
-    public float z() {
-        return p.z();
+    public SphericalPoint toSpherical(float r) {
+        float phi = radians(lng);
+        float theta = map(radians(lat), -PI/2, PI/2, 0, PI);
+        return new SphericalPoint(r, phi, theta);
     }
 }
