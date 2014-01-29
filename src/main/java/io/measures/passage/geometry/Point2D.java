897f6ab87c898befdcb5287fe9f76a2a8b01fb6e
Dietrich Featherston <d@github.com>
2014-01-28 17:35:11 -0800

    add 2d point class

---
+++ src/main/java/io/measures/passage/geometry/Point2D.java
@@ -0,0 +1,9 @@
+package io.measures.passage.geometry;
+
+/**
+ * Point2D
+ *
+ * @author Dietrich Featherston
+ */
+public class Point2D {
+}
---
+++ src/main/java/io/measures/passage/geometry/Projectable2D.java
@@ -0,0 +1,9 @@
+package io.measures.passage.geometry;
+
+/**
+ * Projectable2D
+ *
+ * @author Dietrich Featherston
+ */
+public class Projectable2D {
+}
