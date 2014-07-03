c2fa41a95cadf4b2ff62693e64201609dd48ed8d
Dietrich Featherston <d@d2fn.com>
2014-07-03 00:45:51 -0700

    call base interaction events before delegating to current card

--- src/main/java/com/d2fn/passage/cards/CardSketch.java
+++ src/main/java/com/d2fn/passage/cards/CardSketch.java
@@ -12,7 +12,7 @@ import java.awt.event.MouseWheelEvent;
  */
 public abstract class CardSketch extends Sketch {
 
-    protected Card<?, ?> card;
+    protected Card card;
 
     @Override
     public void renderFrame() {
@@ -24,91 +24,109 @@ public abstract class CardSketch extends Sketch {
 
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
+        super.mouseWheelMoved(e);
         card.mouseWheelMoved(e);
     }
 
     @Override
     public void mousePressed() {
+        super.mousePressed();
         card.mousePressed();
     }
 
     @Override
     public void mousePressed(MouseEvent event) {
+        super.mousePressed(event);
         card.mousePressed(event);
     }
 
     @Override
     public void mouseReleased() {
+        super.mouseReleased();
         card.mouseReleased();
     }
 
     @Override
     public void mouseReleased(MouseEvent event) {
+        super.mouseReleased(event);
         card.mouseReleased(event);
     }
 
     @Override
     public void mouseClicked() {
+        super.mouseClicked();
         card.mouseClicked();
     }
 
     @Override
     public void mouseClicked(MouseEvent event) {
+        super.mouseClicked(event);
         card.mouseClicked(event);
     }
 
     @Override
     public void mouseDragged() {
+        super.mouseDragged();
         card.mouseDragged();
     }
 
     @Override
     public void mouseDragged(MouseEvent event) {
+        super.mouseDragged(event);
         card.mouseDragged(event);
     }
 
     @Override
     public void mouseMoved() {
+        super.mouseMoved();
         card.mouseMoved();
     }
 
     @Override
     public void mouseMoved(MouseEvent event) {
+        super.mouseMoved(event);
         card.mouseMoved(event);
     }
 
     @Override
     public void mouseEntered() {
+        super.mouseEntered();
         card.mouseEntered();
     }
 
     @Override
     public void mouseEntered(MouseEvent event) {
+        super.mouseEntered(event);
         card.mouseEntered(event);
     }
 
     @Override
     public void mouseExited() {
+        super.mouseExited();
         card.mouseExited();
     }
 
     @Override
     public void mouseExited(MouseEvent event) {
+        super.mouseExited(event);
         card.mouseExited(event);
     }
 
     @Override
     public void mouseWheel() {
+        super.mouseWheel();
         card.mouseWheel();
     }
 
     @Override
     public void mouseWheel(MouseEvent event) {
+        super.mouseWheel(event);
         card.mouseWheel(event);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
+        super.keyPressed(e);
         card.keyPressed(e);
     }
 }
